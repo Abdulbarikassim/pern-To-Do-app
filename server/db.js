cd3fc0f5afa01b6cdec2c7e276af7661a5e93972
= <=>
2024-09-07 18:04:47 +0300

    update the database

--- server/db.js
+++ server/db.js
@@ -1,10 +1,22 @@
 import dotenv from "dotenv";
-import pkg from "pg";
+import path from "path";
+import { fileURLToPath } from "url";
+import pg from "pg";
 
-dotenv.config();
+// Resolve __dirname in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const { Pool } = pkg;
+// Load environment variables from .env file
+dotenv.config({
+  override: true,
+  path: path.join(__dirname, ".env"),
+});
 
+// Extract Pool from pg
+const { Pool } = pg;
+
+// Create a new Pool instance using environment variables
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -12,5 +24,11 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
+
+// Test the connection
+pool
+  .connect()
+  .then(() => console.log("Connected to PostgreSQL"))
+  .catch((err) => console.error("Connection error", err.stack));
 
 export default pool;
