cfdc9aae4206795b01574bea16af0d1bd5cdfc0b
= <=>
2024-09-10 09:51:17 +0300

    Added inputTodos components

--- client/src/components/InputTodos.jsx
+++ client/src/components/InputTodos.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InputTodos = () => {
+  const [description, setDescription] = useState("");
+  //  we need to submit the form to send the data out.
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:8000/todo", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      console.log(response);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   return (
     <div className="text-center">
+      f
       <h1 className=" text-3xl font-bold text-green-600 mt-5 p-12 ">
         Pern Todo
       </h1>
-      <form>
+      <form onSubmit={onSubmitForm}>
         <input
           type="text"
           placeholder="Enter you're Todo "
           className="text-center border-solid border-2 border-grey-400 rounded-lg large m-4 p-2  w-1/3"
+          value={description}
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
         />
         <button
           type="submit"
